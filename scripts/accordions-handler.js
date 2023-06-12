94273b40696f955205fc48f456747bdb687fbc1f
Sebastian Pikand <sebastianpikand@gmail.com>
2023-06-12 16:40:16 +0300

    2001

--- scripts/accordions-handler.js
+++ scripts/accordions-handler.js
@@ -1,13 +1,13 @@
-var acc = document.getElementsByClassName("accordion");
-var i;
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function () {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
+var acc = document.getElementsByClassName('accordion');
+
+for (let i = 0; i < acc.length; i++) {
+  acc[i].addEventListener('click', (e) => {
+    e.target.classList.toggle('active');
+    var panel = e.target.nextElementSibling;
+    if (panel.style.display === 'block') {
+      panel.style.display = 'none';
     } else {
-      panel.style.display = "block";
+      panel.style.display = 'block';
     }
   });
 }
--- scripts/contact-form-submit.js
+++ scripts/contact-form-submit.js
@@ -1,34 +1,32 @@
 function submitForm() {
-  let formData = new FormData(document.getElementById("contact-form"));
+  let formData = new FormData(document.getElementById('contact-form'));
   if (checkform() == true) {
-    fetch("https://app.headlessforms.cloud/api/v1/form-submission/B3z2pz6exM", {
-      method: "POST",
+    fetch('https://app.headlessforms.cloud/api/v1/form-submission/B3z2pz6exM', {
+      method: 'POST',
       body: formData,
     }).catch((error) => console.log(error));
   }
 }
 
 function checkform() {
-  const form = document.getElementById("contact-form");
-  let inputs = form.getElementsByTagName("input");
+  const requiredFormFields = ['firstname', 'lastname', 'email', 'message'];
+
+  // const formElements = document.getElementsByClassName('form-element');
+
+  const form = document.getElementById('contact-form');
+
+  let inputs = form.getElementsByTagName('input');
   for (let i = 0; i < inputs.length; i++) {
-    if (inputs[i].hasAttribute("required")) {
-      if (inputs[i].value == "") {
-        return false;
-      }
-    }
+    // if (inputs[i].hasAttribute('required') && inputs[i].value == '') return false;
+    if (requiredFormFields.includes(inputs[i].name) && inputs[i].value == '') return false;
   }
 
-  inputs = form.getElementsByTagName("textarea");
+  inputs = form.getElementsByTagName('textarea');
   for (let i = 0; i < inputs.length; i++) {
-    if (inputs[i].hasAttribute("required")) {
-      if (inputs[i].value == "") {
-        return false;
-      }
-    }
+    if (inputs[i].hasAttribute('required') && inputs[i].value == '') return false;
   }
 
-  if (!document.getElementById("gdprconsent").checked) {
+  if (!document.getElementById('gdprconsent').checked) {
     return false;
   }
 
